6e6d5c2c7d25a197816812c6d861892194df36ab
Johnny Proano <jproano22@yahoo.com>
2023-02-28 23:55:31 -0500

    added new element in html for input and button.

--- emoji.js
+++ emoji.js
@@ -1,9 +1,16 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"];
 
-const emojiContainerEl = document.getElementById("emojiContainer");
+const emojiContainerEl = document.getElementById("emoji-container");
 
 for (let i = 0; i < myEmojis.length; i++) {
-  const newSpan = document.createElement("span");
-  newSpan.textContent = myEmojis[i];
-  emojiContainerEl.append(newSpan);
+  const emoji = document.createElement("span");
+  emoji.textContent = myEmojis[i];
+  emojiContainerEl.append(emoji);
 }
+
+const pushBtn = document.getElementById("push-btn");
+
+pushBtn.addEventListener("click", function () {
+  const emojiInput = document.getElementById("emoji-input");
+  console.log(emojiInput.value);
+});
